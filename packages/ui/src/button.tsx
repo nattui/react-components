fa542e11fd5c0d22fef2682b2b4d26b80587dc20
Natt Nguyen <natt.lefevre@gmail.com>
2025-06-17 12:42:28 -0700

    button size

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,10 +1,40 @@
 import { type ComponentProps } from "react"
 import styles from "@/button.module.css"
 
-export interface ButtonProperties extends ComponentProps<"button"> {}
+export interface ButtonProperties extends ComponentProps<"button"> {
+  size?: 32 | 36 | 40 | 48
+}
+
+export const buttonStyles = {
+  base: styles.button,
+  size: {
+    32: styles.button__size_32,
+    36: styles.button__size_36,
+    40: styles.button__size_40,
+    48: styles.button__size_48,
+  },
+}
 
 export function Button(properties: ButtonProperties) {
-  const { className = "", ...rest } = properties
+  const {
+    children = "",
+    className: customClassName = "",
+    size = 36,
+    ...rest
+  } = properties
 
-  return <button className={`${styles.button} ${className}`.trim()} {...rest} />
+  const combinedClassName = `
+    ${buttonStyles.base}
+    ${buttonStyles.size[size]}
+    ${customClassName}
+  `
+    .replaceAll(/\s+/g, " ")
+    .trim()
+
+  return (
+    <button className={combinedClassName} {...rest}>
+      <p>Click me</p>
+      <span>{children}</span>
+    </button>
+  )
 }
