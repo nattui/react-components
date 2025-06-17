edfd3cb06125a5699c3746c9b78b8233eae3e5ea
Natt Nguyen <natt.lefevre@gmail.com>
2025-06-17 13:55:45 -0700

    button variants

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,38 @@ import { Button } from "@nattui/react-components";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Click me</Button>
+    <div className="flex flex-col gap-4 p-16">
+      <p>Size</p>
+      <div className="flex gap-4 items-center">
+        <Button size={32}>Click me</Button>
+        <Button size={36}>Click me</Button>
+        <Button size={40}>Click me</Button>
+        <Button size={48}>Click me</Button>
+      </div>
+
+      <p>Variant</p>
+      <div className="flex gap-4 items-center">
+        <Button variant="primary">Click me</Button>
+        <Button variant="secondary">Click me</Button>
+        <Button variant="ghost">Click me</Button>
+        <Button variant="accent">Click me</Button>
+      </div>
+
+      <p>Rounded</p>
+      <div className="flex gap-4 items-center">
+        <Button rounded variant="primary">
+          Click me
+        </Button>
+        <Button rounded variant="secondary">
+          Click me
+        </Button>
+        <Button rounded variant="ghost">
+          Click me
+        </Button>
+        <Button rounded variant="accent">
+          Click me
+        </Button>
+      </div>
     </div>
   );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,16 +2,34 @@ import { type ComponentProps } from "react"
 import styles from "@/button.module.css"
 
 export interface ButtonProperties extends ComponentProps<"button"> {
+  fullWidth?: boolean
+  isLoading?: boolean
+  rounded?: boolean
   size?: 32 | 36 | 40 | 48
+  variant?: "primary" | "secondary"
 }
 
 export const buttonStyles = {
   base: styles.button,
+  rounded: {
+    base: styles.button__rounded_base,
+    full: styles.button__rounded_full,
+  },
   size: {
     32: styles.button__size_32,
     36: styles.button__size_36,
     40: styles.button__size_40,
     48: styles.button__size_48,
+  },
+  variant: {
+    accent: styles.button__variant_accent,
+    ghost: styles.button__variant_ghost,
+    primary: styles.button__variant_primary,
+    secondary: styles.button__variant_secondary,
+  },
+  width: {
+    base: styles.button__width_base,
+    full: styles.button__width_full,
   },
 }
 
@@ -19,21 +37,34 @@ export function Button(properties: ButtonProperties) {
   const {
     children = "",
     className: customClassName = "",
+    disabled = false,
+    fullWidth = false,
+    isLoading = false,
+    rounded = false,
     size = 36,
+    type = "button",
+    variant = "primary",
     ...rest
   } = properties
 
   const combinedClassName = `
     ${buttonStyles.base}
     ${buttonStyles.size[size]}
+    ${buttonStyles.variant[variant]}
+    ${rounded ? buttonStyles.rounded.full : buttonStyles.rounded.base}
+    ${fullWidth ? buttonStyles.width.full : buttonStyles.width.base}
     ${customClassName}
   `
     .replaceAll(/\s+/g, " ")
     .trim()
 
   return (
-    <button className={combinedClassName} {...rest}>
-      <p>Click me</p>
+    <button
+      className={combinedClassName}
+      disabled={disabled || isLoading}
+      type={type}
+      {...rest}
+    >
       <span>{children}</span>
     </button>
   )
