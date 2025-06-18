34a44198212ccced4e510f67e05c926724b3736f
Natt Nguyen <natt.lefevre@gmail.com>
2025-06-18 04:52:42 -0700

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@nattui/react-components";
-import { LucideArrowLeft, LucideArrowRight } from "lucide-react";
-
 export default function Home() {
   return <div className="flex flex-col gap-4 p-16"></div>;
 }
