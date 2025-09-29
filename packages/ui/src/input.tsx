c8b5b958822064188f2ccbebf78346f3af932b8e
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-28 23:01:50 -0700

    chore: add prettier plugins to package.json and pnpm-lock.yaml for enhanced code formatting; simplify InputProps interface in input.tsx

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,14 +1,6 @@
 import type { ComponentProps, JSX } from "react"
 
-export interface InputProps extends ComponentProps<"input"> {
-
-
-
-
-
-
-
-}
+export interface InputProps extends ComponentProps<"input"> {}
 
 export default function Input(props: InputProps): JSX.Element {
   return <input {...props} />
