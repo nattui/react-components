02b2408e6baff82068882098acdd6841eecfb576
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-28 23:00:07 -0700

    chore: update @nattui/prettier-config to version 0.0.6 in package.json and pnpm-lock.yaml; remove unused prettier dependency and clean up .prettierignore

---
+++ packages/ui/src/input.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps, JSX } from "react"
+
+export interface InputProps extends ComponentProps<"input"> {
+
+
+
+
+
+
+
+}
+
+export default function Input(props: InputProps): JSX.Element {
+  return <input {...props} />
+}
