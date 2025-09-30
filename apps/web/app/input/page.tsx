e39a5f1eca77b1813ce05200591ec2b449c8d1b5
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-29 20:46:32 -0700

    feat: implement Input component with styles and update InputPage to utilize new Input component for enhanced user input experience

--- apps/web/app/input/page.tsx
+++ apps/web/app/input/page.tsx
@@ -1,3 +1,13 @@
+import { Input } from "@nattui/react-components"
+
 export default function InputPage() {
-  return <div>InputPage</div>
+  return (
+    <div className="flex flex-col gap-4 p-16">
+      <label htmlFor="name">Name</label>
+      <Input id="name" placeholder="Enter your name" />
+      <label htmlFor="email">Email</label>
+      <Input id="email" placeholder="Enter your email" />
+      <Input disabled placeholder="Disabled" />
+    </div>
+  )
 }
--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,7 +1,21 @@
 import type { ComponentProps, JSX } from "react"
+import styles from "@/input.module.css"
 
 export interface InputProps extends ComponentProps<"input"> {}
 
-export default function Input(props: InputProps): JSX.Element {
-  return <input {...props} />
+export function Input(props: InputProps): JSX.Element {
+  const { className: customClassName = "" } = props
+
+  const combinedClassName = `
+    ${INPUT_CLASS_NAME.BASE}
+    ${customClassName}
+  `
+    .replaceAll(/\s+/g, " ")
+    .trim()
+
+  return <input className={combinedClassName} {...props} />
 }
+
+export const INPUT_CLASS_NAME = {
+  BASE: styles.input,
+} as const
