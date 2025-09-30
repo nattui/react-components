15de32022d4742aa420d5d61f60afe3a7f170eb6
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-29 21:32:34 -0700

    feat: remove README.md and add Label component with styles for improved input accessibility

--- apps/web/app/input/page.tsx
+++ apps/web/app/input/page.tsx
@@ -1,14 +1,23 @@
-import { Input } from "@nattui/react-components"
+import { Input, Label } from "@nattui/react-components"
 
 export default function InputPage() {
   return (
-    <div className="flex flex-col gap-4 p-16">
-      <label htmlFor="name">Name</label>
-      <Input id="name" placeholder="Enter your name" />
-      <label htmlFor="email">Email</label>
-      <Input id="email" placeholder="Enter your email" />
-      <Input isDisabled placeholder="Disabled" />
-      <label htmlFor="password">Password</label>
+    <div className="flex flex-col p-16">
+      <Label className="mb-1" htmlFor="name">
+        Name
+      </Label>
+      <Input className="mb-8" id="name" placeholder="Enter your name" />
+
+      <Label className="mb-1" htmlFor="email">
+        Email
+      </Label>
+      <Input className="mb-8" id="email" placeholder="Enter your email" />
+
+      <Input className="mb-8" isDisabled placeholder="Disabled" />
+
+      <Label className="mb-1" htmlFor="password">
+        Password
+      </Label>
       <Input id="password" placeholder="••••••••" type="password" />
     </div>
   )
