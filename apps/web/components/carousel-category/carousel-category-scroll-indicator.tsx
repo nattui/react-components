971e8f2e4ca89702a959772383d667b39b56806a
Natt <natt.lefevre@gmail.com>
2026-02-05 21:13:34 -0800

    refactor: update CarouselCategoryScrollIndicator styles to enhance layout responsiveness

--- apps/web/components/carousel-category/carousel-category-scroll-indicator.tsx
+++ apps/web/components/carousel-category/carousel-category-scroll-indicator.tsx
@@ -8,7 +8,7 @@ export function CarouselCategoryScrollIndicator(props: CarouselCategoryScrollInd
 
   return (
     <div
-      className="from-gray-1 pointer-events-none absolute top-0 block h-full w-32 to-transparent transition-opacity data-[is-hidden=true]:opacity-0 data-[variant=end]:-right-px data-[variant=end]:bg-linear-to-l data-[variant=start]:-left-px data-[variant=start]:bg-linear-to-r" // -x-px to fix mobile Safari gap
+      className="from-gray-1 pointer-events-none absolute top-0 block h-full w-32 to-transparent transition-opacity data-[is-hidden=true]:opacity-0 data-[variant=end]:-right-1 data-[variant=end]:bg-linear-to-l data-[variant=start]:-left-1 data-[variant=start]:bg-linear-to-r" // -x-px to fix mobile Safari gap
       data-is-hidden={isHidden}
       data-variant={variant}
     />
