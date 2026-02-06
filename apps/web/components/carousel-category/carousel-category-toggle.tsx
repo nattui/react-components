0a911bdd6fd5478e41cc0edd4d6405eaf353ac21
Natt <natt.lefevre@gmail.com>
2026-02-05 19:15:39 -0800

    refactor: update CarouselCategoryToggle class for improved layout responsiveness

--- apps/web/components/carousel-category/carousel-category-toggle.tsx
+++ apps/web/components/carousel-category/carousel-category-toggle.tsx
@@ -7,16 +7,13 @@ export function CarouselCategoryToggle(props: ToggleProps) {
   const { children, value, ...rest } = props
 
   function onFocus(event: FocusEvent<HTMLButtonElement>) {
-    event.currentTarget.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-    })
+    event.currentTarget.scrollIntoView({ behavior: "smooth" })
   }
 
   return (
     <Toggle
       onFocus={onFocus}
-      className="bg-gray-3 rounded-8 data-[is-selected=true]:bg-gray-12 group focus-visible:outline-primary-9 flex h-32 shrink-0 cursor-pointer scroll-mx-64 items-center justify-center px-8 outline-2 -outline-offset-2 outline-transparent select-none"
+      className="bg-gray-3 rounded-8 data-[is-selected=true]:bg-gray-12 group focus-visible:outline-primary-9 flex h-32 shrink-0 cursor-pointer scroll-mx-128 items-center justify-center px-8 outline-2 -outline-offset-2 outline-transparent select-none"
       value={value}
       {...rest}
     >
