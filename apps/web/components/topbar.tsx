7a90156f91f8f27686966bc8fe91d141376685e5
Natt <natt.lefevre@gmail.com>
2026-03-08 08:26:44 -0700

    style: enhance Topbar component with backdrop blur effect for improved visual aesthetics

--- apps/web/components/topbar.tsx
+++ apps/web/components/topbar.tsx
@@ -4,7 +4,7 @@ import { Logotype } from "@/components/logotype"
 
 export function Topbar() {
   return (
-    <header className="bg-gray-1 sticky top-0 left-0 z-10 flex h-64 w-full px-24">
+    <header className="bg-gray-1/90 sticky top-0 left-0 z-10 flex h-64 w-full px-24 backdrop-blur-[48px]">
       <div className="flex size-full items-center justify-between">
         <Link
           className="-ml-8 flex items-center gap-x-8 p-8 transition-opacity hover:opacity-75"
