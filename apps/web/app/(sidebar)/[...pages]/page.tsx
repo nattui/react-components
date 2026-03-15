59285a21c4a7d0483366a5e91ff77e6348c8e306
Natt <natt.lefevre@gmail.com>
2026-03-15 11:06:43 -0700

    feat: create new sidebar page component with Notion page ID for testing

---
+++ apps/web/app/(sidebar)/[...pages]/page.tsx
@@ -0,0 +1,11 @@
+const NOTION_PAGE_ID = "30eb76f65e6e8038b950fe10d1b6b5ba"
+
+export default function Page() {
+  console.log("NOTION_PAGE_ID", NOTION_PAGE_ID)
+
+  return (
+    <div>
+      <h1>Test 123</h1>
+    </div>
+  )
+}
