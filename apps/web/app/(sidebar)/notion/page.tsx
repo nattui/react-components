7c8c374e4636657c259f4b62bcd5f3988f668dbb
Natt <natt.lefevre@gmail.com>
2026-03-16 22:52:33 -0700

    refactor: simplify layout in NotionPage by removing unnecessary Spacer components for improved readability

--- apps/web/app/(sidebar)/notion/page.tsx
+++ apps/web/app/(sidebar)/notion/page.tsx
@@ -19,29 +19,23 @@ export default async function NotionPage() {
   }).format(new Date(createdTime))
 
   return (
-    <Column>
-      <Spacer height={64} />
+    <Column className="mx-auto max-w-[620px]">
+      {/* Title */}
+      <h1 className="text-36 font-500 leading-[1.2]">{title}</h1>
+      <Spacer height={12} />
 
-      <Column className="mx-auto max-w-[620px]">
-        {/* Title */}
-        <h1 className="text-36 font-500 leading-[1.2]">{title}</h1>
-        <Spacer height={12} />
+      {/* Created Time */}
+      <p className="text-gray-11 text-14 flex items-center gap-x-8">
+        <span>{formattedCreatedTime}</span>
+        <span>·</span>
+        <span>Natt Nguyen</span>
+      </p>
+      <Spacer height={24} />
 
-        {/* Created Time */}
-        <p className="text-gray-11 text-14 flex items-center gap-x-8">
-          <span>{formattedCreatedTime}</span>
-          <span>·</span>
-          <span>Natt Nguyen</span>
-        </p>
-        <Spacer height={24} />
-
-        {/* Content */}
-        {blocks.map((block: NotionBlock, index) => (
-          <NotionBlockContent block={block} blockIndex={index} key={index} />
-        ))}
-      </Column>
-
-      <Spacer height={128} />
+      {/* Content */}
+      {blocks.map((block: NotionBlock, index) => (
+        <NotionBlockContent block={block} blockIndex={index} key={index} />
+      ))}
     </Column>
   )
 }
