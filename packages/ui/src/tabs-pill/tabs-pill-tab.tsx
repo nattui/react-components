8568434124b1384f495d7b2045f6f342998378d2
Natt <natt.lefevre@gmail.com>
2026-03-19 14:48:54 -0700

    refactor: simplify TabsPill, TabsSegmented, and TabsUnderline components by removing unnecessary isNativeButton prop for cleaner code

--- packages/ui/src/tabs-pill/tabs-pill-tab.tsx
+++ packages/ui/src/tabs-pill/tabs-pill-tab.tsx
@@ -2,26 +2,17 @@ import { Tabs as BaseTabs } from "@base-ui/react"
 import { normalizeWhitespace } from "@/utils/normalize-whitespace"
 import styles from "@/tabs-pill/tabs-pill-tab.module.css"
 
-export interface TabsPillTabProps extends Omit<BaseTabs.Tab.Props, "nativeButton"> {
-  isNativeButton?: boolean
-}
+export interface TabsPillTabProps extends BaseTabs.Tab.Props {}
 
 export function TabsPillTab(props: TabsPillTabProps) {
-  const { className: customClassName = "", isNativeButton = true, ...rest } = props
+  const { className: customClassName = "", ...rest } = props
 
   const combinedClassName = normalizeWhitespace(`
     ${TABS_PILL_TAB_CLASS_NAME.BASE}
     ${customClassName}
   `)
 
-  return (
-    <BaseTabs.Tab
-      className={combinedClassName}
-      data-slot="tabs-tab"
-      nativeButton={isNativeButton}
-      {...rest}
-    />
-  )
+  return <BaseTabs.Tab className={combinedClassName} data-slot="tabs-tab" {...rest} />
 }
 
 export const TABS_PILL_TAB_CLASS_NAME = {
--- packages/ui/src/tabs-segmented/tabs-segmented-tab.tsx
+++ packages/ui/src/tabs-segmented/tabs-segmented-tab.tsx
@@ -2,26 +2,19 @@ import { Tabs as BaseTabs } from "@base-ui/react"
 import { normalizeWhitespace } from "@/utils/normalize-whitespace"
 import styles from "@/tabs-segmented/tabs-segmented-tab.module.css"
 
-export interface TabsSegmentedTabProps extends Omit<BaseTabs.Tab.Props, "nativeButton"> {
+export interface TabsSegmentedTabProps extends BaseTabs.Tab.Props {
   isNativeButton?: boolean
 }
 
 export function TabsSegmentedTab(props: TabsSegmentedTabProps) {
-  const { className: customClassName = "", isNativeButton = true, ...rest } = props
+  const { className: customClassName = "", ...rest } = props
 
   const combinedClassName = normalizeWhitespace(`
     ${TABS_SEGMENTED_TAB_CLASS_NAME.BASE}
     ${customClassName}
   `)
 
-  return (
-    <BaseTabs.Tab
-      className={combinedClassName}
-      data-slot="tabs-tab"
-      nativeButton={isNativeButton}
-      {...rest}
-    />
-  )
+  return <BaseTabs.Tab className={combinedClassName} data-slot="tabs-tab" {...rest} />
 }
 
 export const TABS_SEGMENTED_TAB_CLASS_NAME = {
