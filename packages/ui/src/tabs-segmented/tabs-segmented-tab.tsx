7ee10bbdea6147d9d22f0d1fee2b043148d08b84
Natt <natt.lefevre@gmail.com>
2026-03-19 15:05:10 -0700

    refactor: update TabsPill, TabsSegmented, and TabsUnderline components to include isNativeButton prop for better customization

--- packages/ui/src/tabs-segmented/tabs-segmented-tab.tsx
+++ packages/ui/src/tabs-segmented/tabs-segmented-tab.tsx
@@ -2,19 +2,26 @@ import { Tabs as BaseTabs } from "@base-ui/react"
 import { normalizeWhitespace } from "@/utils/normalize-whitespace"
 import styles from "@/tabs-segmented/tabs-segmented-tab.module.css"
 
-export interface TabsSegmentedTabProps extends BaseTabs.Tab.Props {
+export interface TabsSegmentedTabProps extends Omit<BaseTabs.Tab.Props, "nativeButton"> {
   isNativeButton?: boolean
 }
 
 export function TabsSegmentedTab(props: TabsSegmentedTabProps) {
-  const { className: customClassName = "", ...rest } = props
+  const { className: customClassName = "", isNativeButton = true, ...rest } = props
 
   const combinedClassName = normalizeWhitespace(`
     ${TABS_SEGMENTED_TAB_CLASS_NAME.BASE}
     ${customClassName}
   `)
 
-  return <BaseTabs.Tab className={combinedClassName} data-slot="tabs-tab" {...rest} />
+  return (
+    <BaseTabs.Tab
+      className={combinedClassName}
+      data-slot="tabs-tab"
+      nativeButton={isNativeButton}
+      {...rest}
+    />
+  )
 }
 
 export const TABS_SEGMENTED_TAB_CLASS_NAME = {
--- packages/ui/src/tabs-underline/tabs-underline-tab.tsx
+++ packages/ui/src/tabs-underline/tabs-underline-tab.tsx
@@ -2,17 +2,26 @@ import { Tabs as BaseTabs } from "@base-ui/react"
 import { normalizeWhitespace } from "@/utils/normalize-whitespace"
 import styles from "@/tabs-underline/tabs-underline-tab.module.css"
 
-export interface TabsUnderlineTabProps extends BaseTabs.Tab.Props {}
+export interface TabsUnderlineTabProps extends Omit<BaseTabs.Tab.Props, "nativeButton"> {
+  isNativeButton?: boolean
+}
 
 export function TabsUnderlineTab(props: TabsUnderlineTabProps) {
-  const { className: customClassName = "", ...rest } = props
+  const { className: customClassName = "", isNativeButton = true, ...rest } = props
 
   const combinedClassName = normalizeWhitespace(`
     ${TABS_UNDERLINE_TAB_CLASS_NAME.BASE}
     ${customClassName}
   `)
 
-  return <BaseTabs.Tab className={combinedClassName} data-slot="tabs-tab" {...rest} />
+  return (
+    <BaseTabs.Tab
+      className={combinedClassName}
+      data-slot="tabs-tab"
+      nativeButton={isNativeButton}
+      {...rest}
+    />
+  )
 }
 
 export const TABS_UNDERLINE_TAB_CLASS_NAME = {
