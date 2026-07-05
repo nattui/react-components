41f4ebe8f2d56fc691957909014d8abe04f1f4ec
Natt <natt.lefevre@gmail.com>
2026-07-04 20:49:13 -0700

    feat: enhance Button2 component with new size and rounded variants, and update test route to showcase additional button styles

--- apps/web/src/routes/test.tsx
+++ apps/web/src/routes/test.tsx
@@ -5,10 +5,30 @@ export const Route = createFileRoute("/test")({
   component: function RouteComponent() {
     return (
       <Column className="gap-y-8">
-        <Button2 className="">Click me</Button2>
-        <Button2 className="" isDisabled>
-          Disabled
+        <Button2>Click me</Button2>
+        <Button2 isDisabled>Disabled</Button2>
+
+        <Button2 isRounded size={32}>
+          Size 32
         </Button2>
+        <Button2 isRounded size={36}>
+          Size 36
+        </Button2>
+        <Button2 isRounded size={40}>
+          Size 40
+        </Button2>
+        <Button2 isRounded size={44}>
+          Size 44
+        </Button2>
+        <Button2 isRounded size={48}>
+          Size 48
+        </Button2>
+
+        <Button2 size={32}>Size 32</Button2>
+        <Button2 size={36}>Size 36</Button2>
+        <Button2 size={40}>Size 40</Button2>
+        <Button2 size={44}>Size 44</Button2>
+        <Button2 size={48}>Size 48</Button2>
       </Column>
     )
   },
--- packages/ui/src/components/button-2/button-2.tsx
+++ packages/ui/src/components/button-2/button-2.tsx
@@ -1,22 +1,51 @@
 // oxlint-disable react/button-has-type
 
-import { cva } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority"
 import type { ComponentProps, JSX } from "react"
 import styles from "./button-2.module.css"
 
 interface Button2Props extends Omit<ComponentProps<"button">, "disabled"> {
-  children: number | number[] | string | string[]
+  children?: number | number[] | string | string[]
   isDisabled?: ComponentProps<"button">["disabled"]
+  isRounded?: VariantProps<typeof button2>["isRounded"]
+  size?: VariantProps<typeof button2>["size"]
 }
 
 export function Button2(props: Button2Props): JSX.Element {
-  const { children, className, isDisabled = false, type = "button", ...rest } = props
+  const {
+    children = "",
+    className = "",
+    isDisabled = false,
+    isRounded = false,
+    type = "button",
+    size = 40,
+    ...rest
+  } = props
 
   return (
-    <button className={button2({ className })} disabled={isDisabled} type={type} {...rest}>
+    <button
+      className={button2({ className, isRounded, size })}
+      disabled={isDisabled}
+      type={type}
+      {...rest}
+    >
       <span>{children}</span>
     </button>
   )
 }
 
-const button2 = cva(styles.base)
+const button2 = cva(styles.base, {
+  variants: {
+    isRounded: {
+      false: styles.rounded_base,
+      true: styles.rounded_full,
+    },
+    size: {
+      32: styles.size_32,
+      36: styles.size_36,
+      40: styles.size_40,
+      44: styles.size_44,
+      48: styles.size_48,
+    },
+  },
+})
