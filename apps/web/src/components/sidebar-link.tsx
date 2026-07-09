6c2f4cba9cffe966c8b73594e141e72edea5592a
Natt <natt.lefevre@gmail.com>
2026-07-08 18:34:48 -0700

    style: adjust font weight in SidebarLink component for improved visual consistency

--- apps/web/src/components/sidebar-link.tsx
+++ apps/web/src/components/sidebar-link.tsx
@@ -13,9 +13,9 @@ export function SidebarLink(props: LinkComponentProps): JSX.Element {
         `,
       }}
       className="
-        text-14 font-500
+        text-14
         hover:text-text-primary hover:bg-gray-3
-        rounded-8 flex h-32 shrink-0 items-center px-8
+        rounded-8 flex h-32 shrink-0 items-center px-8 font-[450]
       "
       {...props}
     />
