66129c5f8d1ea20acd71eef2a0ae6a16c7551d93
Natt <natt.lefevre@gmail.com>
2026-07-26 15:50:16 -0700

    refactor: update SelectProps interface to support generic types for enhanced flexibility

--- packages/ui/src/components/select/select.tsx
+++ packages/ui/src/components/select/select.tsx
@@ -1,9 +1,12 @@
 import { Select as BaseSelect } from "@base-ui/react"
 import type { JSX } from "react"
 
-export interface SelectProps extends BaseSelect.Root.Props<string, boolean> {}
+export interface SelectProps<Value, Multiple extends boolean | undefined = false> extends BaseSelect
+  .Root.Props<Value, Multiple> {}
 
-export function Select(props: SelectProps): JSX.Element {
+export function Select<Value, Multiple extends boolean | undefined = false>(
+  props: SelectProps<Value, Multiple>,
+): JSX.Element {
   const { ...rest } = props
 
   return <BaseSelect.Root data-slot="select" {...rest} />
